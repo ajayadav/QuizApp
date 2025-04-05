f05826e729bb3a3ff69a3296b41761ff5df12576
ajayadav <ajayadav88@gmail.com>
2025-04-05 22:38:13 +0530

    Delete src directory

--- src/Actions/type.js
+++
@@ -1,6 +0,0 @@
-export const LOAD_QUIZ = 'LOAD_QUIZ';
-
-
-export const SUBMIT_QUESTION = 'SUBMIT_QUESTION';
-export const SKIP_QUESTION = 'SKIP_QUESTION';
-export const SELECT_OPTION = 'SELECT_OPTION';
